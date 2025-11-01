fd6eed209db360e7533403c8ba462b49ca0b4b16
Hegy <45619085+hegyok@users.noreply.github.com>
2025-11-01 09:10:45 +0100

    blabla

--- init.lua
+++ init.lua
@@ -15,6 +15,21 @@ require("core.lazy")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
 
+-- Buffer navigation
+vim.keymap.set("n", "<leader><Left>", ':bprevious<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader><Right>", ':bnext<CR>', { noremap = true, silent = true })
+
+-- Close current buffer
+vim.keymap.set('n', '<leader>x', ':hide<CR>', { noremap = true, silent = true })
+
+-- New vertical split
+vim.keymap.set('n', '<leader>n', ':vsplit<CR>', { noremap = true, silent = true })
+
+-- New horizontal split 
+vim.keymap.set('n', '<leader>nv', ':split<CR>', { noremap = true, silent = true })
+
+-- Open copilot chat
+vim.keymap.set('n', '<leader>chat', ':CopilotChatOpen<CR>', { noremap = true, silent = true })
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
